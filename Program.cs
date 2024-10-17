b05091a91d86aa8543724fee2fd31b9b81b4ca93
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:38:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericQueue
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            //Performaing enqueue...
+            
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            Console.WriteLine("Enqueue............");
+          
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            //Dequeue.......
+            Console.WriteLine("Dequeue");
+            var d =q.Dequeue();
+            Console.WriteLine(d);
+            
+            
+            
+        }
+    }
+}
